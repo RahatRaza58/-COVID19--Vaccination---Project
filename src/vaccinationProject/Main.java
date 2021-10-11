c289f5ccedd51e66022ba378b6218dc6542b3597
RahatRaza58 <rahatraza58@gmail.com>
2021-10-11 13:06:27 +0600

    File Handling & Exception Added

--- src/vaccinationProject/Main.java
+++ src/vaccinationProject/Main.java
@@ -4,35 +4,50 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    String identity;
 
     public static void main(String[] args) throws FileNotFoundException {
 
 
         System.out.println("Welcome to Vaccination Registration Process ");
-        VaccinationDate vaccinationDate= new VaccinationDate();
+        VaccinationDate vaccinationDate = new VaccinationDate();
 
 
         //int a = 0;
-        System.out.println("Please select Your Occupation- ");
-        System.out.println("1. Student");
-        System.out.println("2. Teacher ");
-        System.out.println("3. Employer");
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nAre you member Of Leading University(Yes/No): ");
+        String identity = input.next();
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter your Choose (1-3): ");
-        int n = in.nextInt();
+        if (identity.equals("Yes")) {
+            System.out.println("Please select Your Occupation- ");
+            System.out.println("1. Student");
+            System.out.println("2. Teacher ");
+            System.out.println("3. Employer");
 
-        if (n == 1)
-        {
-            StudentInfo studentInfo1 = new StudentInfo();
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter your Choose (1-3): ");
+            int n = in.nextInt();
+
+            if (n == 1) {
+                StudentInfo studentInfo1 = new StudentInfo();
+            } else if (n == 2) {
+                TeacherInfo teacherInfo = new TeacherInfo();
+            } else if (n == 3) {
+                EmployeeInfo employeeInfo = new EmployeeInfo();
+            }
+            else
+            {
+                System.out.println("*************Invalid Integer Value Selected*****************");
+            }
         }
-        else if (n == 2)
+        else if (identity.equals("No"))
         {
-            TeacherInfo teacherInfo=new TeacherInfo();
+            System.out.println("Sorry You are not Permit");
         }
-        else if (n == 3)
+        else
         {
-            EmployeeInfo employeeInfo= new EmployeeInfo();
+            System.out.println("******************Invalid Input Type*****************************");
         }
     }
 }
+
--- src/vaccinationProject/StudentInfo.java
+++ src/vaccinationProject/StudentInfo.java
@@ -1,12 +1,149 @@
 package vaccinationProject;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.Scanner;
 
 public class StudentInfo {
     StudentInfo() throws FileNotFoundException {
-        //int a = 0;
-        VaccinationDate vaccinationDate = new VaccinationDate();
-        System.out.println("StudentInfo class-");
 
+        VaccinationDate vaccinationDate = new VaccinationDate();
+        System.out.println("\nStudentInfo class-");
+        int a = 0;
+        String name, department,section;
+        int id, batch;
+        try {
+            Formatter formatter2 = new Formatter("C:/COVID19  Vaccination  Project/DataEntryList/StudentsInfo.txt");
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter Your Department: ");
+            department = input.next();
+            System.out.print("Enter Student ID: ");
+            id = input.nextInt();
+            System.out.print("Enter name: ");
+            name = input.next();
+
+    // Computer Science & Engineering
+        if (department.equals("CSE")) {
+                a = vaccinationDate.time(20);
+                System.out.print("Enter Your Batch: ");
+                batch = input.nextInt();
+                if (batch == 53) {
+                    System.out.print("Enter Your Section: ");
+                    section = input.next();
+                    if (section.equals("A"))
+                    {
+                        System.out.println("\nThank You," + name + " .\nYou Registration is now complete.");
+                        System.out.println("Your Vaccination Date is 25 October");
+                        System.out.println("You must be join this day 08.00am to 08.30am.");
+                    }
+                    else if (section.equals("B"))
+                    {
+                        System.out.println("\nThank You," + name + ".\nYou Registration is complete");
+                        System.out.println("Your Vaccination Date is 25 October");
+                        System.out.println("You must be join this day 08.30pm to 09.00pm.");
+                    }
+                    else if (section.equals("C")) {
+                        System.out.println("\nThank You," + name + ".You Registration is complete");
+                        System.out.println("Your Vaccination Date is 26 October");
+                        System.out.println("You must be join this day 09.00am to 09.30am.");
+                    }
+                    else if (section.equals("D")) {
+                        System.out.println("\nThank You," + name + " .You Registration is complete");
+                        System.out.println("Your Vaccination Date is "+a+" October");
+                        System.out.println("You must be join this day 09.30pm to 10.00pm.");
+                    }
+                    else if (section.equals("E")) {
+                        System.out.println("\nThank You," + name + " .You Registration is complete");
+                        System.out.println("Your Vaccination Date is 27 October");
+                        System.out.println("You must be join this day 10.00am to 10.30am.");
+                    }
+                    else if (section.equals("F")) {
+                        System.out.println("\nThank You," + name + " .You Registration is complete");
+                        System.out.println("Your Vaccination Date is 27 October");
+                        System.out.println("You must be join this day 10.30pm to 11.00pm.");
+                    }
+                    else if (section.equals("G")) {
+                        System.out.println("\nThank You," + name + " .You Registration is complete");
+                        System.out.println("Your Vaccination Date is 28 October");
+                        System.out.println("You must be join this day 11.00am to 11.30am.");
+                    }
+                    else {
+                        System.out.println("Sorry Section didn't match");
+                    }
+                }
+                else if (batch == 52)
+                {
+                    System.out.println("\nThank You," +name+ ".You Registration is complete");
+                    System.out.println("Your Vaccination Date is "+a+" October");
+                    System.out.println("You must be join this day 12.00am to 12.30am.");
+                }
+                else if (batch==51)
+                {
+                    System.out.println("\nThank You," +name+ ".You Registration is complete");
+                    System.out.println("Your Vaccination Date is "+a+" October");
+                    System.out.println("You must be join this day 12.30am to 01.00am.");
+
+                }
+                else if (batch==50)
+                {
+                    System.out.println("\nThank You," +name+ ".You Registration is complete");
+                    System.out.println("Your Vaccination Date is "+a+" October");
+                    System.out.println("You must be join this day 01.30am to 02.00am.");
+
+                }
+                else if (batch==49)
+                {
+                    System.out.println("\nThank You," +name+ ".You Registration is complete");
+                    System.out.println("Your Vaccination Date is "+a+" October");
+                    System.out.println("You must be join this day 02.00am to 02.30am.");
+                }
+                else
+                {
+                    System.out.println("Sorry Your Department or  Batch number didn't match");
+                }
+
+            }
+            formatter2.format("%s %s %s", department, id, name);
+            formatter2.close();
+
+
+        } catch(
+                FileNotFoundException e){
+            System.out.println(e);
+        }
+        try {
+            File file = new File("C:/COVID19  Vaccination  Project/DataEntryList/StudentsInfo.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+
+
+                String fulldepartment = scanner.next();
+                String fullid = scanner.next();
+                String fullname = scanner.next();
+
+
+                System.out.println();
+                System.out.println(" *****************************");
+                System.out.println("Vaccine Application Card");
+                System.out.println("Name: " + fullname);
+                System.out.print("Department: " + fulldepartment);
+                System.out.print("\nStudent ID: " + fullid);
+                //System.out.println("\nDate:" + a + "September");
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/YYYY");
+                Date date=new Date();
+                System.out.print("\nApplication Date: "+simpleDateFormat.format(date));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+
+        }
     }
+
 }
+
+
